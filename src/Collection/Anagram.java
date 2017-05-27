4304eeef87705a20c6f69cfd5668c06212cf623b
Pedro <stargod09@gmail.com>
2017-05-27 20:18:58 +0700

    Collection

--- src/Collection/Anagram.java
+++ src/Collection/Anagram.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package Task1;
+package Collection;
 
 import java.util.Arrays;
 import java.util.HashSet;
